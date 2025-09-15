ecc7bcfc16da751b95179e6d6015fb2f85e34129
mikha <31388146+MikhaD@users.noreply.github.com>
2025-09-15 18:56:04 +0200

    feat: :wrench: force git to use ssh

--- modules/home-manager/git.nix
+++ modules/home-manager/git.nix
@@ -12,6 +12,10 @@
         prune = true;
         pruneTags = true;
       };
+      # Enforce SSH for GitHub
+      url = {
+        "git@github.com:".insteadOf = "https://github.com/";
+      };
       pager.branch = false;
       init.defaultBranch = "main";
       core.autocrlf = "input";
