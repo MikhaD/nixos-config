85fdc1a0973bc584893f6114ad4928016d14b632
mikha <31388146+MikhaD@users.noreply.github.com>
2025-11-22 21:03:43 +0200

    feat: :sparkles: make cb alias work for copy & paste

--- modules/home-manager/wl-clipboard.nix
+++ modules/home-manager/wl-clipboard.nix
@@ -3,7 +3,8 @@
     wl-clipboard
   ];
   programs.bash.shellAliases = {
-    cb = "wl-copy"; # make cb an alias that lets me pipe data to the clipboard
-    sort-cb = "wl-paste | sort | wl-copy"; # Sort the content of the clipboard
+    # [[ -t 0 ]] is true if stdin is a terminal (i.e. data is NOT being piped in)
+    cb = "([[ ! -t 0 ]] && wl-copy) || wl-paste"; # Make cb an alias that lets me pipe data to the clipboard, or paste from it
+    sort-cb = "wl-paste | sort | wl-copy"; #        Sort the content of the clipboard
   };
 }
