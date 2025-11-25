9a9e0220d86b24c3b46c98a937ea092c60546e13
mikha <31388146+MikhaD@users.noreply.github.com>
2025-11-25 21:24:10 +0200

    chore: wrench: start using flake parts & update

--- flake.nix
+++ flake.nix
@@ -3,8 +3,13 @@
     emulators.url = "path:./pkgs/emulators";
     emulators.inputs.nixpkgs.follows = "nixpkgs";
 
+    flake-parts.url = "github:hercules-ci/flake-parts";
+
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    myApps.url = "path:./pkgs";
+    myApps.inputs.nixpkgs.follows = "nixpkgs";
 
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
@@ -14,9 +19,6 @@
     nix-on-droid.url = "github:/nix-community/nix-on-droid";
     nix-on-droid.inputs.nixpkgs.follows = "nixpkgs";
     nix-on-droid.inputs.home-manager.follows = "home-manager";
-
-    myApps.url = "path:./pkgs";
-    myApps.inputs.nixpkgs.follows = "nixpkgs";
 
     # Secrets I don't want in a public repo, but are not secret enough that they must be hidden from the global nix store
     secrets.url = "git+ssh://git@github.com/MikhaD/nix-secrets.git?shallow=1";
@@ -33,41 +35,52 @@
       timeZone = "Africa/Johannesburg";
       flakePath = "/home/${username}/nix";
     };
-    forAllSystems = inputs.nixpkgs.lib.genAttrs [
-      # Add more system architectures here as needed
-      "x86_64-linux"
-      "aarch64-linux"
-    ];
     myLib = import ./lib {inherit (inputs.nixpkgs) lib;};
-  in {
-    nixosConfigurations =
-      builtins.readDir ./hosts/nixos
-      |> builtins.mapAttrs (
-        hostname: _:
-          inputs.nixpkgs.lib.nixosSystem {
-            specialArgs = {inherit details inputs hostname myLib;};
-            modules = [
-              ./modules/base.nix
-              (./. + "/hosts/nixos/${hostname}/configuration.nix")
-            ];
-          }
-      );
-    nixOnDroidConfigurations.default = inputs.nix-on-droid.lib.nixOnDroidConfiguration {
-      pkgs = import inputs.nixpkgs {system = "aarch64-linux";};
-      extraSpecialArgs = {inherit details inputs myLib;};
-      modules = [
-        ./hosts/nix-on-droid/phone/configuration.nix
-      ];
-    };
-    # inspired by https://github.com/librephoenix/nixos-config/blob/95e3002ce7e5519f8692d1903b7cff7110d99c00/flake.nix#L146
-    apps = forAllSystems (system: {
-      default = {
-        type = "app";
-        program = toString ./install.sh;
-        meta.description = "Install script to set up a system using this config";
+  in
+    inputs.flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux" "aarch64-linux"];
+      flake = {
+        nixosConfigurations =
+          builtins.readDir ./hosts/nixos
+          |> builtins.mapAttrs (
+            hostname: _:
+              inputs.nixpkgs.lib.nixosSystem {
+                specialArgs = {inherit details inputs hostname myLib;};
+                modules = [
+                  ./modules/base.nix
+                  (./. + "/hosts/nixos/${hostname}/configuration.nix")
+                ];
+              }
+          );
+        homeConfigurations =
+          builtins.readDir ./hosts/nixos
+          |> builtins.mapAttrs (
+            hostname: _:
+              inputs.home-manager.lib.homeManagerConfiguration {
+                pkgs = import inputs.nixpkgs {system = "x86_64-linux";};
+                specialArgs = {inherit details inputs hostname myLib;};
+                modules = [
+                  (./. + "/hosts/nixos/${hostname}/home.nix")
+                ];
+              }
+          );
+        nixOnDroidConfigurations.default = inputs.nix-on-droid.lib.nixOnDroidConfiguration {
+          pkgs = import inputs.nixpkgs {system = "aarch64-linux";};
+          extraSpecialArgs = {inherit details inputs myLib;};
+          modules = [
+            ./hosts/nix-on-droid/phone/configuration.nix
+          ];
+        };
       };
-    });
-    # tell nix which formatter to use when you run nix fmt <filename/dir>
-    formatter = forAllSystems (system: inputs.nixpkgs.legacyPackages.${system}.alejandra);
-  };
+      perSystem = {pkgs, ...}: {
+        apps.default = {
+          # inspired by https://github.com/librephoenix/nixos-config/blob/95e3002ce7e5519f8692d1903b7cff7110d99c00/flake.nix#L146
+          type = "app";
+          program = toString ./install.sh;
+          meta.description = "Install script to set up a system using this config";
+        };
+        # tell nix which formatter to use when you run nix fmt <filename/dir>
+        formatter = pkgs.alejandra;
+      };
+    };
 }
