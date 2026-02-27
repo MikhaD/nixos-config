2b1fe1a3ffe0f1eb5874a59b6a495c8f3670e9af
mikha <31388146+MikhaD@users.noreply.github.com>
2026-02-27 09:27:56 +0200

    feat: :wrench: add mcp servers to claude code

--- modules/home-manager/claude.nix
+++ modules/home-manager/claude.nix
@@ -1,5 +1,26 @@
-{...}: {
+{inputs,lib,pkgs, ...}: {
   programs.claude-code = {
     enable = true;
+    mcpServers = {
+      svelte = {
+        type = "http";
+        url = "https://mcp.svelte.dev/mcp";
+      };
+      serena = {
+        type = "stdio";
+        # need to use getExe' because serena flake does not specify a meta property
+        command = lib.getExe' inputs.serena.packages.${pkgs.stdenv.system}.default "serena";
+        args = [
+          "start-mcp-server"
+          "--context=claude-code"
+          "--project-from-cwd"
+        ];
+      };
+    };
+  };
+
+  home.sessionVariables = {
+    # Avoid sending all tool descriptions to Claude upon startup, thus saving tokens. Instead, Claude will search for tools as needed (but there are no guarantees that it will search optimally, of course).
+    ENABLE_TOOL_SEARCH = "true";
   };
 }
